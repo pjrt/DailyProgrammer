ffeb49e1010f9f4fc231574805f5f2a9c0a42c07
Pedro Rodriguez <vondiefinsternis@gmail.com>
2014-01-24 22:22:14 -0500

    Sport points

---
+++ Easy/SportPoints.hs
@@ -0,0 +1,25 @@
+-- http://www.reddit.com/r/dailyprogrammer/comments/1undyd/010714_challenge_147_easy_sport_points/
+
+import System.Environment(getArgs)
+
+getCombinations :: Int -> [(Int, Int, Int, Int)]
+getCombinations intPoints =
+        [ (w, x, y, z) |
+        w <- [0..intPoints],
+        x <- [0..intPoints],
+        y <- [0..intPoints],
+        z <- [0..intPoints],
+        8*w + x*7 + y*6 + z*3 == intPoints
+        ]
+
+isValidPoints :: Int -> Bool
+isValidPoints points =
+        let combinations = getCombinations points
+        in not $ null combinations
+
+main = do
+    (points:_) <- getArgs
+    isValid <- return $ isValidPoints $ read points
+    if isValid
+        then print "Valid Score"
+        else print "Invalid Score"
