f450ff62ae3c0856634f750ec56f4899f6ad0e87
Pedro Rodriguez <vondiefinsternis@gmail.com>
2014-01-30 00:21:40 -0500

    Added GraphRadius. Needed massive help with this one. Used /u/ooesili's algo (found in the thread). Copied a lot of his/her names and overall algo, but kept myself from looking at the direct code too much.

---
+++ Intermediate/GraphRadius.hs
@@ -0,0 +1,43 @@
+-- http://www.reddit.com/r/dailyprogrammer/comments/1tiz4z/122313_challenge_140_intermediate_graph_radius/
+
+import System.Environment
+import Data.List
+import Debug.Trace(traceShow)
+import Data.Maybe(maybeToList)
+
+traceS :: Show a => String -> a -> a
+traceS s a = traceShow (s ++ show a) a
+
+type Node = Int
+type Graph = [(Node, [Node])]
+type Matrix = [[Node]]
+
+main = do
+    (size:graphStr:_) <- getArgs
+    let matrix = buildMatrix graphStr
+        graph = buildGraph (read size) matrix
+        allNodes = map fst graph
+        radius = minimum $ map (findEcc graph allNodes) allNodes
+    putStrLn $ show radius
+
+buildMatrix :: String -> Matrix
+buildMatrix = map (map read) . map words . lines
+
+buildGraph :: Int -> Matrix -> Graph
+buildGraph size = zip [0..] . map go
+    where go line = map snd $ filter ((==1) . fst) $ line `zip` [0.. (size - 1)]
+
+expandView :: Graph -> [Node] -> [Node]
+expandView graph nodes = nub $ concatMap go nodes
+    where go :: Node -> [Node]
+          go node = concat $ maybeToList $ lookup node graph
+
+findEcc :: Graph -> [Node] -> Node -> Int
+findEcc graph allNodes node = findEcc' allNodes [] [node] 0
+    where findEcc' notSeen seen currentView depth =
+            let seen' = seen ++ currentView
+                currentView' = expandView graph currentView
+                notSeen' = notSeen \\ (seen' ++ currentView')
+            in if null notSeen
+                 then depth
+                 else findEcc' notSeen' seen' currentView' $ depth + 1
